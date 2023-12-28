c3921ff68f9ccc1eaf684b100fe4d73d2bd234ae
newmaster <2338946313@qq.com>
2023-12-28 20:36:21 +0800

    add OrdersController function page;

--- src/main/java/com/newmaster/reggie/controller/OrdersController.java
+++ src/main/java/com/newmaster/reggie/controller/OrdersController.java
@@ -1,14 +1,14 @@
 package com.newmaster.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.newmaster.reggie.common.R;
 import com.newmaster.reggie.pojo.Orders;
 import com.newmaster.reggie.service.OrdersService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -27,5 +27,22 @@ public class OrdersController {
         log.info("订单数据:{}", orders);
         ordersService.submit(orders);
         return R.success("下单成功");
+    }
+
+    /**
+     * 订单信息分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(Integer page,Integer pageSize, String number) {
+        log.info("接收到的分页参数为:page:{};pageSize:{};number:{}", page, pageSize, number);
+        Page pageInfo = new Page<>(page, pageSize);
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper();
+        queryWrapper.eq(StringUtils.isNotEmpty(number), Orders::getId, number);
+        queryWrapper.orderByDesc(Orders::getOrderTime);
+        ordersService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
     }
 }
