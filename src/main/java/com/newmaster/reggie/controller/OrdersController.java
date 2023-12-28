9d07f8277eb16fb5f1221129a6fa201a222d3a8f
newmaster <2338946313@qq.com>
2023-12-28 23:45:38 +0800

    update OrdersController ====> page

--- src/main/java/com/newmaster/reggie/controller/OrdersController.java
+++ src/main/java/com/newmaster/reggie/controller/OrdersController.java
@@ -3,12 +3,19 @@ package com.newmaster.reggie.controller;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.newmaster.reggie.common.R;
+import com.newmaster.reggie.dto.OrdersDto;
 import com.newmaster.reggie.pojo.Orders;
+import com.newmaster.reggie.pojo.User;
 import com.newmaster.reggie.service.OrdersService;
+import com.newmaster.reggie.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -16,6 +23,9 @@ import org.springframework.web.bind.annotation.*;
 public class OrdersController {
     @Autowired
     private OrdersService ordersService;
+
+    @Autowired
+    private UserService userService;
 
     /**
      * 用户下单
@@ -38,11 +48,31 @@ public class OrdersController {
     @GetMapping("/page")
     public R<Page> page(Integer page,Integer pageSize, String number) {
         log.info("接收到的分页参数为:page:{};pageSize:{};number:{}", page, pageSize, number);
-        Page pageInfo = new Page<>(page, pageSize);
+
+        Page<Orders> pageInfo = new Page<>(page, pageSize);
+        Page<OrdersDto> ordersDtoPage = new Page<>(page, pageSize);
+
         LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper();
         queryWrapper.eq(StringUtils.isNotEmpty(number), Orders::getId, number);
         queryWrapper.orderByDesc(Orders::getOrderTime);
         ordersService.page(pageInfo, queryWrapper);
-        return R.success(pageInfo);
+
+        BeanUtils.copyProperties(pageInfo, ordersDtoPage, "records");
+        List<Orders> records = pageInfo.getRecords();
+        List<OrdersDto> list = records.stream().map((item) -> {
+            OrdersDto ordersDto = new OrdersDto();
+            BeanUtils.copyProperties(item, ordersDto);
+            Long userId = item.getUserId();
+            User user = userService.getById(userId);
+            if (user != null) {
+                String userName = user.getName();
+                ordersDto.setUserName(userName);
+            }
+            return ordersDto;
+        }).collect(Collectors.toList());
+
+        ordersDtoPage.setRecords(list);
+
+        return R.success(ordersDtoPage);
     }
 }
