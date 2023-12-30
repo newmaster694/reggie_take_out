b6745bfb9df7a630e9fed8193f00360e593ab69b
newmaster <2338946313@qq.com>
2023-12-30 22:07:57 +0800

    update

--- src/test/java/com/newmaster/reggie/ReggieApplicationTests.java
+++ src/test/java/com/newmaster/reggie/ReggieApplicationTests.java
@@ -2,12 +2,25 @@ package com.newmaster.reggie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import redis.clients.jedis.Jedis;
 
 @SpringBootTest
 class ReggieApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testRedis() {
+        //获取连接
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+
+        //执行具体的操作
+        jedis.set("username", "newmaster");
+        String username = jedis.get("username");
+        System.out.println(username);
+
+        jedis.del("username");
+
+        //关闭连接
+        jedis.close();
     }
 
 }
